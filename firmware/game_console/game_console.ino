b4c7093f8f613fecd857fe6e171f121081e948f0
arsuhinars <arsuhinars@gmail.com>
2022-12-28 13:06:27 +0400

    Added test code

--- firmware/game_console/game_console.ino
+++ firmware/game_console/game_console.ino
@@ -1,10 +1,32 @@
-#include <EncButton.h>
+#include "controls.hpp"
+#include "display.hpp"
+#include "menu.hpp"
+
+#define FPSTR(pstr) (const __FlashStringHelper*)(pstr)
+
+const char item_1[] PROGMEM = "Змейка";
+const char item_2[] PROGMEM = "Pong";
+const char item_3[] PROGMEM = "Dino";
+const char item_4[] PROGMEM = "Тетрис";
+
+const char title[] PROGMEM = "Игра";
+
+const __FlashStringHelper* items[] = {
+  FPSTR(item_1), FPSTR(item_2), FPSTR(item_3), FPSTR(item_4)
+};
+
+Menu main_menu = Menu(items, 4, FPSTR(title));
 
 void setup() {
-  
+  Serial.begin(9600);
+
+  // Инициализируем экран 
+  display::init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Обновляем ввод
+  controls::update();
 
+  main_menu.update();
 }
