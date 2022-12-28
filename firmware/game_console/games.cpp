9b61c3efa56b4ca73af795016eb24dc4892812e3
arsuhinars <arsuhinars@gmail.com>
2022-12-28 13:55:46 +0400

    Added new games

--- firmware/game_console/games.cpp
+++ firmware/game_console/games.cpp
@@ -1,12 +1,21 @@
 #include "games.hpp"
 
+// Подключаем классы игр
+#include "snake.hpp"
+#include "pong.hpp"
+#include "dino.hpp"
+
 // Массив, содержащий все игры
 const GameBuilder games[] = {
   // addGame<КЛАСС ИГРЫ>(),
+  addGame<Snake>(),
+  addGame<Pong>(),
+  addGame<Dino>(),
 };
 
 // Массив с названиями игр
 const char* const games_names[] = {
+  SNAKE_NAME, PONG_NAME, DINO_NAME
 };
 
 const uint8_t games_count = sizeof(games) / sizeof(GameBuilder);
