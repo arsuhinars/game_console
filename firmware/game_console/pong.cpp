447e020117e1774d76b1a8938daa9234c9bde7bd
arsuhinars <arsuhinars@gmail.com>
2022-12-30 16:55:25 +0400

    Game Pong released

--- firmware/game_console/pong.cpp
+++ firmware/game_console/pong.cpp
@@ -1,11 +1,317 @@
+#include <Arduino.h>
+
+#include "controls.hpp"
 #include "display.hpp"
+#include "utils.hpp"
+#include "texts.hpp"
 #include "pong.hpp"
 
-Pong::Pong() {
+#define BALL_SPRITE_SIZE 9
 
+// Спрайт мяча
+const uint8_t ball_sprite[] PROGMEM = {
+	0x38, 0xFE, 0xFE, 0xFF, 0xFF, 0xFF, 0xFE, 0xFE, 0x38, 
+	0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 
+};
+
+const __FlashStringHelper* MENU_ITEMS[] = {
+  FPSTR(texts::ONE_PLAYER), FPSTR(texts::TWO_PLAYERS), FPSTR(texts::QUIT)
+};
+
+Pong::Pong() : _menu() {
+  _menu = new Menu(
+    MENU_ITEMS, 3, FPSTR(texts::PONG_NAME)
+  );
 }
 
-void Pong::update() {
-  display::oled.home();
-  display::oled.print("Pong");
+Pong::~Pong() {
+  delete _menu;
+}
+
+bool Pong::update() {
+  bool need_redraw = false;
+  int16_t old_ball_x = _ball_x;
+  int16_t old_ball_y = _ball_y;
+
+  switch (_game_data.state) {
+    case GameState::MAIN_MENU:
+      _menu->update();
+
+      if (_menu->clickedItem() == 2) {
+        return false;
+      } else if (_menu->clickedItem() != -1) {
+        _game_data.type = 
+          _menu->clickedItem() == 0 ?
+          GameType::ONE_PLAYER :
+          GameType::TWO_PLAYERS;
+        _game_data.left_score = 0;
+        _game_data.right_score = 0;
+
+        _menu->handleClickedItem();
+
+        startNewRound();
+      }
+      return true;
+    case GameState::IDLE:
+    case GameState::IDLE_REDRAW:
+      if (millis() - _last_time > IDLE_TIME) {
+        _game_data.state = GameState::ROUND;
+        return true;
+      }
+      break;
+    case GameState::ROUND:
+      if (millis() - _last_time > UPDATE_STEP) {
+        _last_time = millis();
+        need_redraw = true;
+        
+        // Ограничиваем скорость мяча
+        _ball_vel_x = constrain(_ball_vel_x, -BALL_MAX_SPEED, BALL_MAX_SPEED);
+        _ball_vel_y = constrain(_ball_vel_y, -BALL_MAX_SPEED, BALL_MAX_SPEED);
+
+        // Обновляем положение мяча
+        _ball_x += _ball_vel_x * UPDATE_STEP / 1000;
+        _ball_y += _ball_vel_y * UPDATE_STEP / 1000;
+
+        int16_t ball_left = _ball_x / SUBPIXELS_COUNT - BALL_SPRITE_SIZE / 2;
+        int16_t ball_right = _ball_x / SUBPIXELS_COUNT + BALL_SPRITE_SIZE / 2;
+        int16_t ball_top = _ball_y / SUBPIXELS_COUNT - BALL_SPRITE_SIZE / 2;
+        int16_t ball_bottom = _ball_y / SUBPIXELS_COUNT + BALL_SPRITE_SIZE / 2;
+
+        int16_t left_racket_top = _left_racket_pos / SUBPIXELS_COUNT - RACKET_LENGTH / 2;
+        int16_t left_racket_bottom = _left_racket_pos / SUBPIXELS_COUNT + RACKET_LENGTH / 2;
+
+        int16_t right_racket_top = _right_racket_pos / SUBPIXELS_COUNT - RACKET_LENGTH / 2;
+        int16_t right_racket_bottom = _right_racket_pos / SUBPIXELS_COUNT + RACKET_LENGTH / 2;
+
+        // Обрабатываем столкновение мяча с верхней и нижней стенкой
+        if ( ball_top <= 0 || ball_bottom >= DISPLAY_HEIGHT - 1) {
+          _ball_vel_y *= -1;
+        }
+
+        // Обрабатываем столкновения с левой ракеткой
+        if (ball_left <= RACKET_THICKNESS - 1 && (
+          (ball_top <= left_racket_bottom && ball_top >= left_racket_top) ||
+          (ball_bottom <= left_racket_bottom && ball_bottom >= left_racket_top)
+        )) {
+          _ball_x = (RACKET_THICKNESS + BALL_SPRITE_SIZE / 2) * SUBPIXELS_COUNT;
+          _ball_vel_x *= -1;
+          _ball_vel_y += _left_racket_vel;
+        }
+
+        // Обрабатываем столковения с правой ракеткой
+        if (ball_right >= DISPLAY_WIDTH - RACKET_THICKNESS && (
+          (ball_top <= right_racket_bottom && ball_top >= right_racket_top) ||
+          (ball_bottom <= right_racket_bottom && ball_bottom >= right_racket_top)
+        )) {
+          _ball_x = (DISPLAY_WIDTH - 1 - RACKET_THICKNESS - BALL_SPRITE_SIZE / 2) * SUBPIXELS_COUNT;
+          _ball_vel_x *= -1;
+          _ball_vel_y += _left_racket_vel;
+        }
+
+        // Обработка выхода за границы экрана
+        if (ball_left <= 0) {
+          ++_game_data.right_score;
+          startNewRound();
+        } else if (ball_right >= DISPLAY_WIDTH - 1) {
+          ++_game_data.left_score;
+          startNewRound();
+        }
+
+        // Считываем движение левой ракетки
+        int16_t left_racket_accel = 0;
+        if (controls::up_button.state()) {
+          left_racket_accel = -RACKET_ACCEL;
+        } else if (controls::down_button.state()) {
+          left_racket_accel = RACKET_ACCEL;
+        }
+
+        // Обновляем левую ракетку
+        _left_racket_pos = utils::smoothMove(
+          _left_racket_pos,
+          _left_racket_vel,
+          left_racket_accel,
+          RACKET_DRAG,
+          UPDATE_STEP
+        );
+
+        // Ограничиваем движение левой ракетки
+        if (left_racket_top < 0) {
+          _left_racket_pos = RACKET_LENGTH * SUBPIXELS_COUNT / 2;
+          _left_racket_vel = 0;
+        } else if (left_racket_bottom >= DISPLAY_HEIGHT) {
+          _left_racket_pos = (DISPLAY_HEIGHT - 1 - RACKET_LENGTH / 2) * SUBPIXELS_COUNT;
+          _left_racket_vel = 0;
+        }
+
+        int16_t right_racket_accel = 0;
+        if (_game_data.type == GameType::ONE_PLAYER) {
+          // Если игра в одиночку, то правая ракетка управляется псевдо ИИ
+          ;
+        }
+        // Если игра вдвоем
+        else if (controls::button_b.state()) {
+          right_racket_accel = -RACKET_ACCEL;
+        } else if (controls::button_a.state()) {
+          right_racket_accel = RACKET_ACCEL;
+        }
+
+        // Обновляем правую ракетку
+        _right_racket_pos = utils::smoothMove(
+          _right_racket_pos,
+          _right_racket_vel,
+          right_racket_accel,
+          RACKET_DRAG,
+          UPDATE_STEP
+        );
+
+        // Ограничиваем движение правой ракетки
+        if (right_racket_top < 0) {
+          _right_racket_pos = RACKET_LENGTH * SUBPIXELS_COUNT / 2;
+          _right_racket_vel = 0;
+        } else if (right_racket_bottom >= DISPLAY_HEIGHT) {
+          _right_racket_pos = (DISPLAY_HEIGHT - 1 - RACKET_LENGTH / 2) * SUBPIXELS_COUNT;
+          _right_racket_vel = 0;
+        }
+      }
+      break;
+  }
+
+  if (_game_data.state == GameState::IDLE_REDRAW) {
+    _game_data.state = GameState::IDLE;
+    display::oled.clear();
+  } else if (need_redraw) {
+    old_ball_x /= SUBPIXELS_COUNT;
+    old_ball_y /= SUBPIXELS_COUNT;
+
+    // Очищаем только те области, которые изменились
+    display::oled.clear(
+      old_ball_x - BALL_SPRITE_SIZE / 2,
+      old_ball_y - BALL_SPRITE_SIZE / 2,
+      old_ball_x + BALL_SPRITE_SIZE / 2,
+      old_ball_y + BALL_SPRITE_SIZE / 2
+    );
+
+    // Очищаем область у левой ракетки
+    display::oled.clear(
+      0,
+      0,
+      RACKET_THICKNESS - 1,
+      _left_racket_pos / SUBPIXELS_COUNT - RACKET_LENGTH / 2
+    );
+    display::oled.clear(
+      0,
+      _left_racket_pos / SUBPIXELS_COUNT + RACKET_LENGTH / 2,
+      RACKET_THICKNESS - 1,
+      DISPLAY_HEIGHT - 1
+    );
+
+    // Аналогично у правой
+    display::oled.clear(
+      DISPLAY_WIDTH - RACKET_THICKNESS,
+      0,
+      DISPLAY_WIDTH - 1,
+      _right_racket_pos / SUBPIXELS_COUNT - RACKET_LENGTH / 2
+    );
+    display::oled.clear(
+      DISPLAY_WIDTH - RACKET_THICKNESS,
+      _right_racket_pos / SUBPIXELS_COUNT + RACKET_LENGTH / 2,
+      DISPLAY_WIDTH - 1,
+      DISPLAY_HEIGHT - 1
+    );
+  } else {
+    // Если не требуется перерисовка
+    return true;
+  }
+
+  // Отрисовываем мяч
+  display::oled.drawBitmap(
+    _ball_x / SUBPIXELS_COUNT - BALL_SPRITE_SIZE / 2,
+    _ball_y / SUBPIXELS_COUNT - BALL_SPRITE_SIZE / 2,
+    ball_sprite,
+    BALL_SPRITE_SIZE,
+    BALL_SPRITE_SIZE,
+    0, BUF_REPLACE
+  );
+
+  // Отрисовываем ракетки
+  display::oled.rect(
+    0,
+    _left_racket_pos / SUBPIXELS_COUNT - RACKET_LENGTH / 2,
+    RACKET_THICKNESS - 1,
+    _left_racket_pos / SUBPIXELS_COUNT + RACKET_LENGTH / 2
+  );
+  display::oled.rect(
+    DISPLAY_WIDTH - RACKET_THICKNESS,
+    _right_racket_pos / SUBPIXELS_COUNT - RACKET_LENGTH / 2,
+    DISPLAY_WIDTH - 1,
+    _right_racket_pos / SUBPIXELS_COUNT + RACKET_LENGTH / 2
+  );
+
+  // Отрисовываем счет
+  display::oled.setCursorXY(DISPLAY_WIDTH / 2 - 15, 0);
+  display::oled.print(_score_text);
+
+  // Обновляем дисплей
+  display::oled.update();
+
+  return true;
+}
+
+void Pong::startNewRound() {
+  // Если одна из сторон победила
+  if (_game_data.left_score >= MAX_SCORE || _game_data.right_score >= MAX_SCORE) {
+    __FlashStringHelper* menu_title;
+    // Меняем заголовок в зависимости от того, кто выиграл
+    if (_game_data.left_score == _game_data.right_score) {
+      menu_title = FPSTR(texts::DRAW);
+    } else if (_game_data.left_score > _game_data.right_score) {
+      menu_title = 
+        _game_data.type == GameType::ONE_PLAYER ?
+        FPSTR(texts::YOU_WIN) :
+        FPSTR(texts::LEFT_WIN);
+    } else {
+      menu_title = 
+        _game_data.type == GameType::ONE_PLAYER ?
+        FPSTR(texts::YOU_LOSE) :
+        FPSTR(texts::RIGHT_WIN);
+    }
+
+    delete _menu;
+    _menu = new Menu(MENU_ITEMS, 3, menu_title, 1);
+    controls::resetStates();
+    _game_data.state = GameState::MAIN_MENU;
+    return;
+  }
+  
+  _game_data.state = GameState::IDLE_REDRAW;
+  _last_time = millis();
+
+  _left_racket_pos = DISPLAY_HEIGHT / 2 * SUBPIXELS_COUNT;
+  _left_racket_vel = 0;
+  _right_racket_pos = DISPLAY_HEIGHT / 2 * SUBPIXELS_COUNT;
+  _right_racket_vel = 0;
+
+  _ball_x = DISPLAY_WIDTH / 2 * SUBPIXELS_COUNT;
+  _ball_y = DISPLAY_HEIGHT  / 2 * SUBPIXELS_COUNT;
+  _ball_vel_x = random(BALL_MAX_SPEED / 2, BALL_MAX_SPEED + 1) * SUBPIXELS_COUNT;
+  _ball_vel_y = random(BALL_MAX_SPEED / 2, BALL_MAX_SPEED + 1) * SUBPIXELS_COUNT;
+  if (random(2)) {
+    _ball_vel_x *= -1;
+  }
+  if (random(2)) {
+    _ball_vel_y *= -1;
+  }
+
+  // Формируем строку с текстом счета
+  _score_text[0] = _game_data.left_score < 10 ? ' ' : '0' + _game_data.left_score / 10;
+  _score_text[1] = '0' + _game_data.left_score % 10;
+  _score_text[2] = '-';
+  if (_game_data.right_score < 10) {
+    _score_text[3] = '0' + _game_data.right_score % 10;
+    _score_text[4] = ' ';
+  } else {
+    _score_text[3] = '0' + _game_data.right_score / 10;
+    _score_text[4] = '0' + _game_data.right_score % 10;
+  }
+  _score_text[5] = '\0';
 }
--- firmware/game_console/pong.hpp
+++ firmware/game_console/pong.hpp
@@ -3,13 +3,69 @@
 #include <avr/pgmspace.h>
 
 #include "game.hpp"
+#include "menu.hpp"
 
-// Храним название игры в Flash памяти
-const char PONG_NAME[] PROGMEM = "Pong";
+// Параметры игры
+#define MAX_SCORE         5     // Максимальный счет
+#define IDLE_TIME         1000  // Время задержки в начале раунда
+#define UPDATE_STEP       30    // Длительность шага обновления игры
+#define SUBPIXELS_COUNT   4     // Количество субпикселей в одном пикселе
+#define BALL_MAX_SPEED    200   // Максимальная скорость мячика
+#define RACKET_ACCEL      600   // Ускорение ракетки
+#define RACKET_DRAG       2     // Коэффициент сопротивления движению ракетки
+#define RACKET_THICKNESS  3     // Толщина ракетки
+#define RACKET_LENGTH     18    // Длина ракетки
 
 class Pong : public Game {
 public:
   explicit Pong();
+  ~Pong();
 
-  void update() override;
+  bool update() override;
+
+private:
+  // Перечисление состояний игры
+  enum class GameState : uint8_t {
+    MAIN_MENU,    // Главное меню
+    IDLE,         // Состояние ожидания между раундами
+    IDLE_REDRAW,  // То же самое, что и пред., требующее перерисовки всего поля
+    ROUND         // Запущенный раунд игры
+  };
+
+  // Типы игры
+  enum class GameType : uint8_t {
+    ONE_PLAYER,   // Игра за одного игрока
+    TWO_PLAYERS,  // Игра вдвоем
+  };
+
+  Menu* _menu = nullptr;
+  
+  // Структура, хранящая данные игры
+  // Для оптимизации памяти используются битовые поля
+  struct {
+    GameState state : 2;
+    GameType type : 1;
+    uint8_t left_score : 4;
+    uint8_t right_score : 4;
+  } _game_data = { GameState::MAIN_MENU, GameType::ONE_PLAYER };
+
+  // Строка с текстом счета
+  char _score_text[6];
+
+  // Позиции и скорости каждой ракетки
+  int16_t _left_racket_pos = 0;
+  int16_t _left_racket_vel = 0;
+  int16_t _right_racket_pos = 0;
+  int16_t _right_racket_vel = 0;
+
+  // Позиция и скорость мячика
+  int16_t _ball_x = 0;
+  int16_t _ball_y = 0;
+  int16_t _ball_vel_x = 0;
+  int16_t _ball_vel_y = 0;
+
+  unsigned long _last_time = 0;
+
+  // Метод запуска нового раунда
+  void startNewRound();
 };
