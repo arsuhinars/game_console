5b7f009cdf7fdd267aa0786fba6b226f96cd32cb
arsuhinars <arsuhinars@gmail.com>
2023-01-01 21:21:10 +0400

    rus_strlen function optimization

--- firmware/game_console/utils.cpp
+++ firmware/game_console/utils.cpp
@@ -3,16 +3,8 @@
 size_t utils::rus_strlen(const char *s) {
   size_t i = 0;
   size_t length = 0;
-  while (true) {
-    uint8_t c = (uint8_t)s[i];
-
-    if (c == '\0') {
-      break;
-    }
-
-    if (c <= 191 && (i == 0 || c != 128 || (uint8_t)s[i - 1] != 226)) {
-      ++length;
-    }
+  while (s[i]) {
+    if ((s[i] & 0xC0) != 0x80) length++;
     ++i;
   }
   return length;
@@ -21,18 +13,9 @@ size_t utils::rus_strlen(const char *s) {
 size_t utils::rus_strlen_P(PGM_P s) {
   size_t i = 0;
   size_t length = 0;
-  while (true) {
-    uint8_t c = pgm_read_byte(&s[i]);
-
-    if (c == '\0') {
-      break;
-    }
-
-    if (c <= 191 && (i == 0 || c != 128 || pgm_read_byte(&s[i - 1]) != 226)) {
-      ++length;
-    }
+  while (pgm_read_byte(&s[i])) {
+    if ((pgm_read_byte(&s[i]) & 0xc0) != 0x80) length++;
     ++i;
   }
   return length;
 }
-
