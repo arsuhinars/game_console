5b68bc3644df57a6b78031d697d18ddaea2e222e
arsuhinars <arsuhinars@gmail.com>
2023-01-23 15:55:19 +0400

    Added Tetris to menu

--- firmware/game_console/games.cpp
+++ firmware/game_console/games.cpp
@@ -1,9 +1,11 @@
+#include "game.hpp"
 #include "games.hpp"
 
 // Подключаем классы игр
 #include "snake.hpp"
 #include "pong.hpp"
 #include "dino.hpp"
+#include "tetris.hpp"
 
 #include "texts.hpp"
 
@@ -13,13 +15,15 @@ const GameCreator games[] = {
   getGameCreator<Snake>(),
   getGameCreator<Pong>(),
   getGameCreator<Dino>(),
+  getGameCreator<Tetris>()
 };
 
 // Массив с названиями игр
 const char* const games_names[] PROGMEM = {
   texts::SNAKE_NAME,
   texts::PONG_NAME,
-  texts::DINO_NAME
+  texts::DINO_NAME,
+  texts::TETRIS_NAME
 };
 
 const uint8_t games_count = sizeof(games) / sizeof(GameCreator);
